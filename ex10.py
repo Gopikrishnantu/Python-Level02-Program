699ebc01c73ac51e1edc4cb4b3c44de2b6e650a7
Gopikrishnantu <ggn9729@gmail.com>
2025-08-04 10:42:40 +0530

    level2 python program

---
+++ ex10.py
@@ -0,0 +1,14 @@
+print("Exponential program")
+print("---------")
+x=int(input("Enter x value"))
+y=int(input("Enter y value"))
+z=int(input("Enter z value"))
+print("Result")
+P=x*x+y*y+z*z
+print("P",P)
+Q=x*x*x+y*y+z+20
+print("Q",Q)
+R=x*x*y+y*y+z*z*x+10
+print("R",R)
+S=x*x*x+y*y*z+x*x*+y*y*+x*y*z
+print("S",S)
---
+++ ex9.py
@@ -0,0 +1,10 @@
+print("sum of n numbers")
+print("--------------")
+n=int(input("Enter the n number:"))
+print("result")
+sm=n*(n+1)/2
+print("sum",sm)
+sq=n*(n+1)*(2*n+1)/6
+print("sum square",sq)
+sc=(n*(n+1)/2)*(n*(n+1)/2)
+print("sum cube",sc)
